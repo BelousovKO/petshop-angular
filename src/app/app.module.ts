7c50f5acbc5dfd8020af5b5d0b59373071e43fc6
belousovko <working.belousovko@gmail.com>
2021-10-03 19:57:12 +0700

    Imported the font, start forming the primary-button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { PrimaryButtonComponent } from './controls/primery-button/primary-button.component';
+import { SecondaryButtonComponent } from './controls/secondary-button/secondary-button.component';
+import { MenuButtonComponent } from './controls/menu-button/menu-button.component';
+import { HeaderSearchComponent } from './controls/header-search/header-search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HeaderComponent,
+    PrimaryButtonComponent,
+    SecondaryButtonComponent,
+    MenuButtonComponent,
+    HeaderSearchComponent
   ],
   imports: [
     BrowserModule,
